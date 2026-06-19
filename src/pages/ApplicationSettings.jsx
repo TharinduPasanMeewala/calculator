d46e8c35c44a769cc84fcad3a822a51b2049636e
TharinduPasanMeewala <saragroupit2@gmail.com>
2026-06-19 11:08:26 +0530

    Add src/pages/ApplicationSettings.jsx

---
+++ src/pages/ApplicationSettings.jsx
@@ -0,0 +1,8 @@
+export default function ApplicationSettingsPage() {
+  return (
+    <div className="p-6">
+      <h1 className="text-2xl font-bold text-gray-900 mb-2">Application Settings</h1>
+      <p className="text-gray-500">This page is ready to be implemented.</p>
+    </div>
+  )
+}
